50bbc85d6d59358fca73f6d5e6c08a787683abe4
yaman <yamantripathi@gmail.com>
2022-04-02 01:33:59 -0700

    Initial Commit

---
+++ src/main/java/com/todo/Application.java
@@ -0,0 +1,10 @@
+package com.todo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
